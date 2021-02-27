d55bf19a5c7f5304391064b566ae082b9a51f969
R Narayan <nr.rnarayan@gmail.com>
2021-02-28 04:04:56 +0530

    added users schema :sparkles:

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Userroute from './Routes/api/User.js';
+import mongoose from 'mongoose';
 const app = express();
 app.use(express.json());
 
@@ -11,7 +12,9 @@ app.use((error, req, res, next) => {
 	}
 	res.status(error.code).json({ message: error.message });
 });
-
+mongoose.connect(
+	'mongodb+srv://pseudochaos:HelloWorld@cluster0.wpqfu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+);
 app.listen(8000, () => {
 	console.log('server running on port 8000');
 });
---
+++ backend/models/Users.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+const schema = mongoose.Schema;
+const UserSchema = new schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	phone: {
+		type: Number,
+		required: true,
+	},
+	address: {
+		type: String,
+		required: true,
+	},
+	aadharno: {
+		type: Number,
+		required: true,
+	},
+	aadharcardphotofront: {
+		type: String,
+		required: true,
+	},
+	aadharcardphotoback: {
+		type: String,
+		required: true,
+	},
+	emergencycontacts: [
+		{
+			type: String,
+			required: true,
+		},
+	],
+});
+const exp = mongoose.model('User', UserSchema);
+export default exp;
