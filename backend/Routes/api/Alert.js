eae25ecc8e55dd894dcbf39c2f57e75a1c016b5a
R Narayan <nr.rnarayan@gmail.com>
2021-02-28 06:27:07 +0530

    Adding alerts :construction:

---
+++ backend/Routes/api/Alert.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import error from '../../models/Error.js';
+import User from '../../models/Users.js';
+import Vol from '../../models/Volunteer.js';
+const router = express.Router();
+router.post('/', async (req, res) => {});
+
+export default router;
